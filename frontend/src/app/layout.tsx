24eadee122baa8287e7da63bbdd2db5ca4e1afd5
Kuber <kuberkhandare@gmail.com>
2026-04-11 12:50:11 +0530

    Fix React Context Provider instantiation in RootLayout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -30,7 +30,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Providers>{children}</Providers>
+        <Providers>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </Providers>
       </body>
     </html>
   );
